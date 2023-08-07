291b6acde6beb7ce1766bf828664a055c3ae8ede
dssathsarani <sewminisathsarani1@gmail.com>
2023-08-07 19:13:07 +0530

    filly completed bus home page

--- lib/pages/bus_schedule/buspage.dart
+++ lib/pages/bus_schedule/buspage.dart
@@ -1,6 +1,6 @@
 import 'package:campus_connect_app/pages/home2.dart';
 import 'package:flutter/material.dart';
-import 'package:vertical_card_pager/vertical_card_pager.dart';
+
 import 'buspage1.dart';
 import 'buspage2.dart';
 import 'buspage3.dart';
