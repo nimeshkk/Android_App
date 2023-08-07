3e6dfbb0b141567c04cf348d2a77ab7d42a7fc47
dssathsarani <sewminisathsarani1@gmail.com>
2023-08-07 18:30:56 +0530

    page updates

--- lib/pages/bus_schedule/buspage3.dart
+++ lib/pages/bus_schedule/buspage3.dart
@@ -1,1 +1,24 @@
 import 'package:flutter/material.dart';
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 253, 252, 255),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 4, 100, 160),
+        shadowColor: const Color.fromARGB(6, 36, 192, 240),
+        title: const Text(
+          "Private Bus Information",
+          style: TextStyle(fontWeight: FontWeight.w900, fontSize: 24),
+        ),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.menu))],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
