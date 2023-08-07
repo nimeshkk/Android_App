a4ea84a0ad21b56b5461d833f3518141dea1095d
jisdulanjana <93591453+jisdulanjana@users.noreply.github.com>
2023-08-07 09:54:20 +0530

    add

---
+++ lib/pages/clubs_and_community.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ClubsAndCommunity extends StatelessWidget {
+  const ClubsAndCommunity({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Clubs and Community')),
+    );
+  }
+}
