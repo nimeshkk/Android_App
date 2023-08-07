fd922f706d79398140639fe09671873d5141a127
dssathsarani <sewminisathsarani1@gmail.com>
2023-08-07 18:44:10 +0530

    edit buspage1

--- lib/pages/bus_schedule/buspage.dart
+++ lib/pages/bus_schedule/buspage.dart
@@ -30,7 +30,62 @@ class Buspage extends StatelessWidget {
           child: Column(
             crossAxisAlignment:
                 CrossAxisAlignment.start, // get the text in rigth hand side
-            children: [],
+            children: [
+              const Text(
+                "NSBM Shuttle bus service",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 26, 24, 24),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height:
+                    30, // to get space between image and topics like text and image
+              ),
+              Center(
+                child: Image.asset(
+                  "assets/shuttle-bus-main-image1.jpg",
+                  height: 300,
+                  scale: 2,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                "Apart from public and private transport services, a limited shuttle service is available during the peak hours between High-Level Road and NSBM Green University.These buses provide transport to students at affordable rates in a pre-defined schedule.The limited shuttle service between High-Level Road and NSBM Green University is a convenient and affordable transportation option exclusively designed for students during peak hours. This service aims to cater to the transportation needs of the students, ensuring they can easily commute between these two locations for their academic pursuits.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromARGB(255, 26, 25, 25),
+                    fontWeight: FontWeight.w400),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              // button 01
+
+              GestureDetector(
+                onTap: () {
+                  // Navigate to Page1 when Button is pressed
+                  Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                    return Page1();
+                  }));
+                },
+                child: Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: const Color.fromARGB(255, 121, 186, 239),
+                    ),
+                    child: const Text(
+                      "Shuttle bus timetable",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/pages/bus_schedule/buspage1.dart
+++ lib/pages/bus_schedule/buspage1.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 245, 245, 248),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 4, 100, 160),
+        shadowColor: const Color.fromARGB(255, 4, 100, 160),
+        title: const Text(
+          "NSBM Shuttle Bus Scheudle",
+          style: TextStyle(fontWeight: FontWeight.w900, fontSize: 24),
+        ),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.menu))],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
