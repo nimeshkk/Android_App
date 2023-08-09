96cd97f4deddbdd6d5715998c50e91b57b2d1bdd
nsewmini <nsewmini2001@gmail.com>
2023-08-09 14:34:55 +0530

    implement the delete update pages

--- lib/pages/upcoming_events/user/addevent.dart
+++ lib/pages/upcoming_events/user/addevent.dart
@@ -48,134 +48,145 @@ class _AddEventScreenState extends State<AddEventScreen> {
             ),
           ),
         ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                      'assets/back.jpg'), // Replace with your image asset
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Selected Date: ${widget.selectedDate.toString()}'),
-                      SizedBox(height: 20),
-                      TextFormField(
-                        onChanged: (value) {
-                          setState(() {
-                            name = value;
-                          });
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Event Name',
-                          hintText: 'Enter event name',
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                  'assets/back.jpg'), // Set your background image asset
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                            'Selected Date: ${widget.selectedDate.toString()}'),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              name = value;
+                            });
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Event Name',
+                            hintText: 'Enter event name',
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      TextFormField(
-                        onChanged: (value) {
-                          setState(() {
-                            description = value;
-                          });
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Description',
-                          hintText: 'Enter event description',
+                        SizedBox(height: 20),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              description = value;
+                            });
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Description',
+                            hintText: 'Enter event description',
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      TextFormField(
-                        onChanged: (value) {
-                          setState(() {
-                            organization = value;
-                          });
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Organization',
-                          hintText: 'Enter event organization',
+                        SizedBox(height: 20),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              organization = value;
+                            });
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Organization',
+                            hintText: 'Enter event organization',
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      TextFormField(
-                        onChanged: (value) {
-                          setState(() {
-                            location = value;
-                          });
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'location',
-                          hintText: 'Enter event location',
+                        SizedBox(height: 20),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              location = value;
+                            });
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'location',
+                            hintText: 'Enter event location',
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () {
-                          widget.addEventCallback(
-                            widget.selectedDate,
-                            name,
-                            date,
-                            description,
-                            organization,
-                            location,
-                          );
-                          Navigator.pop(context);
-                        },
-                        child: Text('Add Event'),
-                      ),
-                      SizedBox(height: 20),
-                      Row(
-                        children: [
-                          ElevatedButton(
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            widget.addEventCallback(
+                              widget.selectedDate,
+                              name,
+                              date,
+                              description,
+                              organization,
+                              location,
+                            );
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            'Add Event',
+                            style: TextStyle(
+                              color:
+                                  Colors.black, // Set the font color to black
+                              fontWeight: FontWeight
+                                  .bold, // Set the font weight to bold
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => DeleteEventScreen(),
+                                    ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Color.fromARGB(255, 216, 43,
+                                      30), // Set the background color to red
+                                ),
+                                child: Text('Delete Event',
+                                    style: TextStyle(
+                                      color: Colors
+                                          .black, // Set the font color to black
+                                      fontWeight: FontWeight
+                                          .bold, // Set the font weight to bold
+                                    ))),
+
+                            Spacer(), // This will push the Update Event button to the right
+                            ElevatedButton(
                               onPressed: () {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                    builder: (context) => DeleteEventScreen(),
+                                    builder: (context) => UpdateEventScreen(),
                                   ),
                                 );
                               },
                               style: ElevatedButton.styleFrom(
-                                backgroundColor: Color.fromARGB(255, 216, 43,
-                                    30), // Set the background color to red
+                                backgroundColor: Color.fromARGB(168, 146, 239,
+                                    96), // Set the background color to red
                               ),
-                              child: Text('Delete Event',
+                              child: Text('Update Event',
                                   style: TextStyle(
                                     color: Colors
                                         .black, // Set the font color to black
                                     fontWeight: FontWeight
                                         .bold, // Set the font weight to bold
-                                  ))),
-
-                          Spacer(), // This will push the Update Event button to the right
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => UpdateEventScreen(),
-                                ),
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color.fromARGB(168, 146, 239,
-                                  96), // Set the background color to red
+                                  )),
                             ),
-                            child: Text('Update Event',
-                                style: TextStyle(
-                                  color: Colors
-                                      .black, // Set the font color to black
-                                  fontWeight: FontWeight
-                                      .bold, // Set the font weight to bold
-                                )),
-                          ),
-                        ],
-                      ),
-                    ]),
+                          ],
+                        ),
+                      ]),
+                ),
               ),
             ),
           ),
