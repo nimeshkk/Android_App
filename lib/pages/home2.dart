da6e3529bdb4e6e7eaca3acda36c21ab281b17bb
nimeshkk <nimeshkliyanage@gmail.com>
2023-08-10 15:49:31 +0530

    update

--- lib/pages/home2.dart
+++ lib/pages/home2.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:campus_connect_app/pages/gpa_pages/gpahome.dart';
 import 'package:campus_connect_app/pages/bus_schedule/buspage.dart';
+import 'package:campus_connect_app/pages/home_renting/home_page.dart';
 import 'package:campus_connect_app/pages/upcoming_events/event_homepage.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
@@ -171,6 +172,15 @@ class _Home2State extends State<Home2> {
                             context,
                             MaterialPageRoute(
                               builder: (_) => ClubsAndCommunity(),
+                            ),
+                          );
+                        }
+
+                        if (imagePath == 'assets/accommodation.png') {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => HomePage(),
                             ),
                           );
                         }
