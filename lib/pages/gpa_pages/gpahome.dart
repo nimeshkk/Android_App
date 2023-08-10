092e0a9f1bd29d8a26de0d22558ebc8b63073d59
dssathsarani <sewminisathsarani1@gmail.com>
2023-08-10 15:06:45 +0530

    add code to gpahome.dart

--- lib/pages/gpa_pages/gpahome.dart
+++ lib/pages/gpa_pages/gpahome.dart
@@ -1,8 +1,122 @@
 import 'package:flutter/material.dart';
+import 'gpapage.dart';
 
 class Gpahome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/gpa9.png",
+              height: MediaQuery.of(context).size.height * 0.55,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+            ),
+            // Image.asset(
+            //   "assets/gpa7.png",
+            //  fit: BoxFit.cover,
+            // ),
+            const SizedBox(height: 10),
+
+            GestureDetector(
+              onTap: () {},
+              child: const Text(
+                "Welcome to GPA Calculator",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 70, 71, 183),
+                    fontSize: 30,
+                    fontWeight: FontWeight.w900),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 30),
+
+            GestureDetector(
+              onTap: () {},
+              child: const Text(
+                "Empower yourself with instant insights into your academic achievements. Get started today and unlock the power of GPA Calculator!",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 70, 71, 183),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 30),
+            //login button
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40,
+                vertical: 5,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MaterialButton(
+                      color: Color.fromARGB(255, 70, 71, 183),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => GpaPage()));
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          "Calculate your GPA",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //register button
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40,
+                vertical: 5,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                          color: Color.fromARGB(255, 70, 71, 183),
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => GpaPage()));
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          "Home",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 70, 71, 183),
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
