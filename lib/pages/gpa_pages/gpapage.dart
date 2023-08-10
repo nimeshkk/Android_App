31787bf3320988f22bb91eae792bfc71f6d2bf02
dssathsarani <sewminisathsarani1@gmail.com>
2023-08-10 14:26:10 +0530

    add classes and code for each pages

---
+++ lib/pages/gpa_pages/gpapage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'gpapage1.dart';
+import 'grade_calculator.dart';
+
+class GpaPage extends StatefulWidget {
+  @override
+  _GpaPageState createState() => _GpaPageState();
+}
+
+class _GpaPageState extends State<GpaPage> {
+  List<Course> courses = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/pages/gpa_pages/gpapage1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'grade_calculator.dart';
+
+class GpaPage1 extends StatefulWidget {
+  @override
+  _GpaPage1State createState() => _GpaPage1State();
+}
+
+class _GpaPage1State extends State<GpaPage1> {
+  String selectedGrade = 'A+';
+  int selectedCredits = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
