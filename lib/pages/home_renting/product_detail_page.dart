e0d1a9f39e414b3fd62c64390ef92d412eadba24
Danindu Dilmina <ddleewanage@students.nsbm.ac.lk>
2023-08-11 13:27:01 +0530

    test 75

--- lib/pages/home_renting/product_detail_page.dart
+++ lib/pages/home_renting/product_detail_page.dart
@@ -10,45 +10,48 @@ class ProductDetaildPage extends StatelessWidget {
     SizeConfig().init(context);
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: GestureDetector(
-        child: Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: kPadding8
-          ),
-          height: 43,
-          width: double.infinity,
-          margin: const EdgeInsets.symmetric(
-            horizontal: kPadding20,
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Price',
-                        style: kRalewayRegular.copyWith(
-                          color: kGrey85,
-                          fontSize: SizeConfig.blockSizeHorizontal! * 2.5,
-                        ),
-                    ),
-                    SizedBox(
-                      height: SizeConfig.blockSizeVertical! * 0.5,
-                    ),
-                    Text(
-                      'Rs 10 000 / Month',
-                        style: kRalewayMedium.copyWith(
-                          color: kBlack,
-                          fontSize: SizeConfig.blockSizeHorizontal! * 4,
-                        ),
-                    ),
-                  ],
-                ),
+      floatingActionButton: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: kPadding8
+        ),
+        height: 43,
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(
+          horizontal: kPadding20,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Price',
+                      style: kRalewayRegular.copyWith(
+                        color: kGrey85,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 2.5,
+                      ),
+                  ),
+                  SizedBox(
+                    height: SizeConfig.blockSizeVertical! * 0.5,
+                  ),
+                  Text(
+                    'Rs 10 000 / Month',
+                      style: kRalewayMedium.copyWith(
+                        color: kBlack,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 4,
+                      ),
+                  ),
+                ],
               ),
-              Container(
-                height: 43.0,
+            ),
+            GestureDetector(
+              onTap: () {
+                debugPrint('Rent Now Tapped');
+              },
+              child: Container(
+                height: 43,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(
                     kBorderRadius10
@@ -58,13 +61,53 @@ class ProductDetaildPage extends StatelessWidget {
                 padding: const EdgeInsets.symmetric(
                   horizontal: kPadding24,
                 ),
-              )
-            ],
-          ),
+                child: Center(
+                  child: Text(
+                    'Rent Now',
+                    style: kRalewaySemiBold.copyWith(
+                      color: kWhite,
+                      fontSize: SizeConfig.blockSizeHorizontal! * 4,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       body: SafeArea(
-        child: SingleChildScrollView(),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(
+            horizontal: kPadding20,
+          ),
+          child: Column(
+            children: [
+              Container(
+                height: 319,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    kBorderRadius20
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      spreadRadius: 0,
+                      color: kBlack.withOpacity(0.1),
+                      blurRadius: 10,
+                      offset: const Offset(0, 18),
+                    ),
+                  ],
+                  image: const DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
+                    ),
+                  ),
+                ),
+              ),   
+            ],
+          ),
+        ),
       ),
     );
   }
