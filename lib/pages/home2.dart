ba5bb127258c8d0bde2a90cde449500eada32429
Chathuka Upamith <95994216+ChathukaU@users.noreply.github.com>
2023-08-18 01:23:44 +0530

    Update home2.dart

--- lib/pages/home2.dart
+++ lib/pages/home2.dart
@@ -7,6 +7,7 @@ import 'package:campus_connect_app/pages/upcoming_events/event_homepage.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'clubs_and_community.dart';
+import 'lost.dart';
 import 'welcome_page.dart';
 
 class Home2 extends StatefulWidget {
@@ -177,6 +178,15 @@ class _Home2State extends State<Home2> {
                           );
                         }
 
+                        if (imagePath == 'assets/Lost2.png') {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => LostAndFound(),
+                            ),
+                          );
+                        }
+                        
                         if (imagePath == 'assets/accommodation.png') {
                           Navigator.push(
                             context,
