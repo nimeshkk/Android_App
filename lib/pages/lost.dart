fa5455c7266c8d548c184805f0a9b6dd4256074b
Chathuka Upamith <95994216+ChathukaU@users.noreply.github.com>
2023-08-18 01:19:08 +0530

    Update lost.dart

--- lib/pages/lost.dart
+++ lib/pages/lost.dart
@@ -1,90 +1,17 @@
-import 'package:flutter/material.dart';
-import 'package:draggable_home/draggable_home.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class LostAndFound extends StatefulWidget {
+  const LostAndFound({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
-        title: "Draggable Home",
-        home: Lost1(),
-      );
+  // ignore: library_private_types_in_public_api
+  _LostAndFoundState createState() => _LostAndFoundState();
 }
 
-class Lost1 extends StatelessWidget {
-  const Lost1({Key? key}) : super(key: key);
-
+class _LostAndFoundState extends State<LostAndFound> {
+  // Implement your Lost and Found page UI and functionality here
   @override
   Widget build(BuildContext context) {
-    return DraggableHome(
-      leading: IconButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        icon: const Icon(Icons.arrow_back_ios),
-      ),
-      title: const Text(""),
-      actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
-      ],
-      headerWidget: headerWidget(context),
-      headerBottomBar: headerBottomBarWidget(),
-      body: [
-        listView(),
-      ],
-      fullyStretchable: true,
-      backgroundColor: Colors.white,
-      appBarColor: Colors.teal,
-    );
-  }
-
-  Row headerBottomBarWidget() {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: const [
-        Icon(
-          Icons.settings,
-          color: Colors.white,
-        ),
-      ],
-    );
-  }
-
-  Widget headerWidget(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Center(
-        child: Text(
-          "Lost & Found",
-          style: Theme.of(context)
-              .textTheme
-              .headline2!
-              .copyWith(color: Colors.white70),
-        ),
-      ),
-    );
-  }
-
-  ListView listView() {
-    return ListView.builder(
-      padding: const EdgeInsets.only(top: 0),
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: 20,
-      shrinkWrap: true,
-      itemBuilder: (context, index) => Card(
-        color: Colors.white70,
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Text("$index"),
-          ),
-          title: const Text("Title"),
-          subtitle: const Text("Subtitle"),
-        ),
-      ),
-    );
+    // Return your Lost and Found page UI
   }
 }
