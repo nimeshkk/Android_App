d623edd0fda4ef663c2bd062e906dbfb41b149e5
nsewmini <nsewmini2001@gmail.com>
2023-08-19 15:57:45 +0530

    change the code

--- lib/pages/timetable/timetable_home.dart
+++ lib/pages/timetable/timetable_home.dart
@@ -1,12 +1,31 @@
 import 'package:campus_connect_app/pages/home2.dart';
 import 'package:flutter/material.dart';
 
+// Define Faculty pages
+class FacultyPage extends StatelessWidget {
+  final String facultyName;
+
+  FacultyPage(this.facultyName);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(facultyName),
+      ),
+      body: Center(
+        child: Text('Timetable information for $facultyName will be displayed here'),
+      ),
+    );
+  }
+}
+
 class Timetable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('time table'),
+        title: Text('Time Table'),
         flexibleSpace: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -21,16 +40,49 @@ class Timetable extends StatelessWidget {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(
-                  builder: (_) =>
-                      Home2()), // Use the correct class name (capitalize the first letter)
+              MaterialPageRoute(builder: (_) => Home2()),
             );
           },
         ),
       ),
       body: Center(
-        child: Text('Timetable information will be displayed in this screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => FacultyPage('Faculty 1')),
+                );
+              },
+              child: Text('Faculty 1'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => FacultyPage('Faculty 2')),
+                );
+              },
+              child: Text('Faculty 2'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => FacultyPage('Faculty 3')),
+                );
+              },
+              child: Text('Faculty 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
