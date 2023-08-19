53f720ab0c6d3f00b8171e68adeebe39bcc7990d
nsewmini <nsewmini2001@gmail.com>
2023-08-20 00:20:40 +0530

    add some

--- lib/pages/timetable/timetable_home.dart
+++ lib/pages/timetable/timetable_home.dart
@@ -1,7 +1,6 @@
 import 'package:campus_connect_app/pages/home2.dart';
 import 'package:flutter/material.dart';
 
-// Define Faculty pages
 class FacultyPage extends StatelessWidget {
   final String facultyName;
 
@@ -14,7 +13,8 @@ class FacultyPage extends StatelessWidget {
         title: Text(facultyName),
       ),
       body: Center(
-        child: Text('Timetable information for $facultyName will be displayed here'),
+        child: Text(
+            'Timetable information for $facultyName will be displayed here'),
       ),
     );
   }
@@ -45,44 +45,86 @@ class Timetable extends StatelessWidget {
           },
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => FacultyPage('Faculty 1')),
-                );
-              },
-              child: Text('Faculty 1'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => FacultyPage('Faculty 2')),
-                );
-              },
-              child: Text('Faculty 2'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => FacultyPage('Faculty 3')),
-                );
-              },
-              child: Text('Faculty 3'),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/timee.png'),
+
+            // Replace with your image path
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  backgroundColor:
+                      Color.fromARGB(255, 96, 251, 53).withOpacity(0.7),
+                  minimumSize: Size(300, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    side: BorderSide(color: Colors.black),
+                  ),
+                  textStyle: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => FacultyPage('Faculty 1')),
+                  );
+                },
+                child: Text('Faculty 1'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  backgroundColor:
+                      Color.fromARGB(255, 1, 154, 255).withOpacity(0.7),
+                  minimumSize: Size(300, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    side: BorderSide(color: Colors.black),
+                  ),
+                  textStyle: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => FacultyPage('Faculty 2')),
+                  );
+                },
+                child: Text('Faculty 2'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  backgroundColor:
+                      Color.fromRGBO(255, 22, 205, 1).withOpacity(0.7),
+                  minimumSize: Size(300, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    side: BorderSide(color: Colors.black),
+                  ),
+                  textStyle: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => FacultyPage('Faculty 3')),
+                  );
+                },
+                child: Text('Faculty 3'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
