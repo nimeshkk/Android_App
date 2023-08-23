f7c71d99293781c31e735871fb80234ab17e394b
nsewmini <nsewmini2001@gmail.com>
2023-08-23 12:34:25 +0530

    add the same code

--- lib/pages/timetable/Business.dart
+++ lib/pages/timetable/Business.dart
@@ -1,3 +1,4 @@
+import 'package:campus_connect_app/pages/timetable/timetable_home.dart';
 import 'package:flutter/material.dart';
 
 class PLYMOUTH extends StatelessWidget {
@@ -27,37 +28,106 @@ class UGC extends StatelessWidget {
     );
   }
 }
+// Import your pages
 
 class BUSINESS extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          // ... (app bar configuration)
+        title: Text('Business Faculty'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF509CF4), Color(0xFF3256C7)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => Timetable()),
+            );
+          },
+        ),
+      ),
       body: Container(
-        // ... (container decoration and other widgets)
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/back01.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
         child: Center(
           child: Column(
-            // ... (other children)
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Text(
+                'Select a Category:',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  backgroundColor:
+                      Color.fromARGB(255, 1, 32, 136).withOpacity(0.9),
+                  minimumSize: Size(300, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    side: BorderSide(color: Colors.black),
+                  ),
+                  textStyle: TextStyle(color: Colors.black),
+                ),
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (_) => PLYMOUTH()),
                   );
                 },
-                child: Text('Plymouth'),
+                child: Text(
+                  'Plymouth',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
+              SizedBox(height: 20),
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  backgroundColor:
+                      Color.fromARGB(255, 69, 1, 57).withOpacity(0.9),
+                  minimumSize: Size(300, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    side: BorderSide(color: Colors.black),
+                  ),
+                  textStyle: TextStyle(color: Colors.black),
+                ),
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (_) => UGC()),
                   );
                 },
-                child: Text('UGC'),
+                child: Text(
+                  'UGC',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
             ],
           ),
--- lib/pages/timetable/Computing.dart
+++ lib/pages/timetable/Computing.dart
@@ -1,4 +1,34 @@
+import 'package:campus_connect_app/pages/timetable/timetable_home.dart';
 import 'package:flutter/material.dart';
+
+class PLYMOUTH extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Plymouth Category'),
+      ),
+      body: Center(
+        child: Text('Plymouth Category Screen Content'),
+      ),
+    );
+  }
+}
+
+class UGC extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UGC Category'),
+      ),
+      body: Center(
+        child: Text('UGC Category Screen Content'),
+      ),
+    );
+  }
+}
+// Import your pages
 
 class COMPUTING extends StatelessWidget {
   @override
@@ -6,10 +36,111 @@ class COMPUTING extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Computing Faculty'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF509CF4), Color(0xFF3256C7)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => Timetable()),
+            );
+          },
+        ),
       ),
-      body: Center(
-        child: Text('Computing Faculty Screen Content'),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/back01.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Select a Category:',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  backgroundColor:
+                      Color.fromARGB(255, 1, 32, 136).withOpacity(0.9),
+                  minimumSize: Size(300, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    side: BorderSide(color: Colors.black),
+                  ),
+                  textStyle: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => PLYMOUTH()),
+                  );
+                },
+                child: Text(
+                  'Plymouth',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 4,
+                  backgroundColor:
+                      Color.fromARGB(255, 69, 1, 57).withOpacity(0.9),
+                  minimumSize: Size(300, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    side: BorderSide(color: Colors.black),
+                  ),
+                  textStyle: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => UGC()),
+                  );
+                },
+                child: Text(
+                  'UGC',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: COMPUTING(),
+    ),
+  );
 }
