5580622b8dbad7749556a27a3e613bb66ffa1078
nimeshkk <nimeshkliyanage@gmail.com>
2023-09-04 12:49:12 +0530

    update

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import 'home2.dart';
 
 
 void main() async {
@@ -50,7 +49,7 @@ class MyApp extends StatelessWidget {
 
 
 
-        home:  Home2()
+        home: SplashScreen()
         );
 
 
