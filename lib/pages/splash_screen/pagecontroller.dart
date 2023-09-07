26eef5501aceb002066c0a5484ed47041cbcb595
dssathsarani <sewminisathsarani1@gmail.com>
2023-09-07 10:11:02 +0530

    ADD SPLASH SCREEN

---
+++ lib/pages/splash_screen/pagecontroller.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class Controller extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WalkthroughScreen(),
+    );
+  }
+}
+
+class WalkthroughScreen extends StatefulWidget {
+  @override
+  _WalkthroughScreenState createState() => _WalkthroughScreenState();
+}
+
+class _WalkthroughScreenState extends State<WalkthroughScreen> {
+  final PageController _controller = PageController(initialPage: 0);
+  int currentPage = 0;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    // Start the auto-slide timer
+    _startAutoSlide();
+  }
+
+  void _startAutoSlide() {
+    // Cancel the previous timer if it exists
+    _timer?.cancel();
+    // Create a new timer that advances the page every 1 second
+    _timer = Timer.periodic(Duration(seconds: 5), (timer) {
+      if (currentPage < 2) {
+        _controller.nextPage(
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+        );
+      } else {
+        // If we're at the last page, cancel the timer
+        timer.cancel();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // Cancel the timer when the widget is disposed
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _controller,
+              onPageChanged: (index) {
+                setState(() {
+                  currentPage = index;
+                });
+              },
+              children: [
+                SliderOne(),
+                SliderTwo(),
+                SliderThree(),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              if (currentPage > 0)
+                ElevatedButton(
+                  onPressed: () {
+                    _controller.previousPage(
+                      duration: Duration(milliseconds: 60),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: Text('Previous'),
+                ),
+              if (currentPage < 2)
+                ElevatedButton(
+                  onPressed: () {
+                    _controller.nextPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                  child: Text('Next'),
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SliderOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/slider01.png'), // Replace with image path
+            fit: BoxFit
+                .cover, // use different BoxFit values depending on your layout requirements
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 180.0), // Adjust margin as needed
+                child: Text(
+                  'Explore clubs, housing options, and more to enhance your university journey',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Color.fromARGB(255, 2, 98, 95),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Move to the next slide or navigate to the next screen
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SliderTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/slider02.png'), // Replace with image path
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(left: 180.0), // Adjust margin as needed
+              child: Text(
+                'Simplify your university experience with our app\'s helpful features',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromARGB(255, 2, 98, 95),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Move to the next slide or navigate to the next screen
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SliderThree extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/slider03.png'), // Replace with image path
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(left: 180.0), // Adjust margin as needed
+              child: Text(
+                'Keep track of bus schedules, classes, and events effortlessly',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromARGB(255, 2, 98, 95),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                //Move to the next slide or navigate to the next screen
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
